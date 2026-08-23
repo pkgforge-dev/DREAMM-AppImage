4071272cce4cfaad1aca21f1dc11fee584b5775b
Link4Electronics <link4electronics@gmail.com>
2026-08-23 20:40:49 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,12 +6,11 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    libdecor \
-    sdl2
+pacman -Syu --noconfirm sdl2
+
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
 echo "Getting app..."
 echo "---------------------------------------------------------------"
